729c2c27dc707d3d1e0666952898cf755f27d758
Beherit <Beherit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-07 09:16:13 +0000

    Reset the password input and span after success registration.
    git-svn-id: https://plugins.svn.wordpress.org/ejabberd-account-tools/trunk@1176121 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/jquery.ejabat.register.validation.js
+++ js/jquery.ejabat.register.validation.js
@@ -221,6 +221,8 @@ jQuery(document).ready(function($) {
 					//Success
 					if(response.status == 'success') {
 						$('#ejabat_register')[0].reset();
+						$('#password input').removeClass('good strong');
+						$('#password span').empty();
 					}
 					$('#response').css('display', 'inline-block');
 					$('#response').addClass('ejabat-register-'+response.status);
